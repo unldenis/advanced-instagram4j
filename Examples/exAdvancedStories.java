ee99764c073b1d84941d1adda79b476472e89646
unldenis <80055679+unldenis@users.noreply.github.com>
2021-03-23 03:30:40 +0100

    Add files via upload

---
+++ Examples/exAdvancedStories.java
@@ -0,0 +1,43 @@
+import com.github.instagram4j.instagram4j.IGClient;
+import com.github.instagram4j.instagram4j.exceptions.IGLoginException;
+
+public class exAdvancedStories {
+
+	public static void main(String[] args) {
+		
+		//Login with Instagram4j
+		IGClient client = null ;
+        try {
+            client = IGClient.builder()
+                    .username("username")
+                    .password("password")
+                    .login();
+        } catch (IGLoginException e) {
+            e.printStackTrace();
+        }
+        
+        
+        AdvancedStories stories = new AdvancedStories(client);  
+        //The user MUST be followed by us
+        stories.loadStoriesUrl("user"); 
+        //Print all images stories urls of user
+        for(Story s : stories.getStories()) 
+        	if(s.isImage())
+        		System.out.println(s);
+        
+        
+        //Load stories of the newest first 5 users to post a story
+        stories.loadStoriesUrl(5); 
+        //Print all usernames and urls loaded
+        for(Story s : stories.getStories()) 
+        	System.out.println(s.getUser().getUsername() + "->" + s.getUrl());
+        
+        
+        
+        System.out.println("End");
+        
+               
+      
+	}
+	
+}
